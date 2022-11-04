ea0f4ab710ad8f7b029468f945ccfdee63ce9666
[TUF] AlexGamer29 <sun123vet@gmail.com>
2022-11-04 11:37:36 +0700

    feat(Home): add customer section for header component

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -9,7 +9,8 @@ const Header = () => {
         <div className="header__title">
           <div className="header__title-primary">
             <p>
-              Utilize to do list to plan your life and oversee the work of your team
+              Utilize to do list to plan your life and oversee the work of your
+              team
             </p>
           </div>
           <div className="header__title-secondary">
@@ -20,10 +21,17 @@ const Header = () => {
           </div>
         </div>
         <div className="app__header-img">
-          <img
-            src={images.headerImage}
-            alt=""
-          ></img>
+          <img src={images.headerImage} alt=""></img>
+        </div>
+        <div className="app__header-customer">
+          <h1 className="customer__title-primary">Join with our 1,000+ customers around the globe</h1>
+          <div className="customer-list">
+            <i class="fa-brands fa-google" />
+            <i class="fa-brands fa-facebook" />
+            <i class="fa-brands fa-youtube" />
+            <i class="fa-brands fa-linkedin-in" />
+            <i class="fa-brands fa-github" />
+          </div>
         </div>
       </div>
     </div>
